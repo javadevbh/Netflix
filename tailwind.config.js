e12b704bb40649d587a38caff5158e201ae4442d
Javad Bahrami <javadev14bh@gmail.com>
2023-08-01 20:41:51 +0330

    Add icons, font and base typography styes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        persian : ['Vazirmatn' , ...defaultTheme.fontFamily.sans],
+      },
+      colors : {
+        primary : {
+          light : "#eaedf6",
+          red : "#c4252c",
+          gray : "#839bb1",
+          lighter : "#fafbff"
+        }
+      }
+    },
   },
   plugins: [],
 }
